119500ac51661b2355950f883c53bc2625e65565
Karunakar <karunakar.kadari@inovar-tech.com>
2026-03-11 12:09:13 +0530

    Revert the index changes

--- packages/ui/src/templates/index.ts
+++ packages/ui/src/templates/index.ts
@@ -1,11 +1,15 @@
+// Templates
 export * from "./admin-layout";
 export * from "./monitor-layout";
+export * from "./executive-layout";
 export * from "./stakeholder-layout";
 export * from "./false-positive-modal";
 export * from "./future-features-section";
 export * from "./reconfigure-modal";
 export * from "./setup-layout";
 export * from "./sidebar-feature-banner";
+
+
 
 // Theme is now exported from the theme module
 // Use: import { ThemeProvider, useTheme } from "ui/theme";
